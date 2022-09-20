e9d3125097e0caf5fc11b27cc9bca450aa77f4c6
Mwininia-Saanyeh <ramoakidas@gmail.com>
2022-09-20 14:49:21 +0000

    express api

---
+++ test/Subtract.test.js
@@ -0,0 +1,7 @@
+const Subtract = require("./Subtract")
+
+
+test("subtract two numbers", () => {
+    expe("Subtract" (50, 12)
+    ).toBe(26)
+})
---
+++ test/Sum.test.js
@@ -0,0 +1,7 @@
+const Sum =require("./Sum")
+
+
+test("accurately add two numbers", () => {
+    expe(Sum (67, 43)
+    ).toBe(110)
+})
